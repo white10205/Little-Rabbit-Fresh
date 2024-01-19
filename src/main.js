9e2f8d318abecdbaaf729071a1d621dec5a09686
沈仁东 <1020572432@qq.com>
2024-01-19 20:53:39 +0800

    axios的基础封装

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import {getCategoryAPI} from '@/apis/text';
+getCategoryAPI().then(res =>{
+    console.log(res);
+})
 const app = createApp(App)
 
 app.use(createPinia())
