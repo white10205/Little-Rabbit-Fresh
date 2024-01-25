f33ceb93a4af250b56d94b633a20629ea57db7a4
沈仁东 <1020572432@qq.com>
2024-01-25 22:04:14 +0800

    商品详情页

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -18,7 +18,7 @@ onMounted(() => {
   <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img v-img-lazy="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">&yen;{{ item.price }}</p>
