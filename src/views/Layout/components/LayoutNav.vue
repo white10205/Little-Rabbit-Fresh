60fb64c762badbade3996b230058c86777dc09e6
沈仁东 <1020572432@qq.com>
2024-01-28 22:08:21 +0800

    退出登录

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { useUserStore } from '@/stores/user';
+import { useRouter } from 'vue-router';
+
 const userStore = useUserStore()
+const router = useRouter()
+const confirm = ()=>{
+  userStore.setUserInfo();
+  router.push({path:'/login'})
+}
 </script>
 
 <template>
@@ -10,9 +17,9 @@ const userStore = useUserStore()
         <template v-if="userStore.userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
-                <a href="javascript:;" @click="userStore.setUserInfo()">退出登录</a>
+                <a href="javascript:;" >退出登录</a>
               </template>
             </el-popconfirm>
           </li>
